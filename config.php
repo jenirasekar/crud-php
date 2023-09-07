bbeeb0df6a47fa89f64e189c4ca13c54e5cae9e1
Jenira Sekar <jenirasekar@gmail.com>
2023-09-07 07:41:11 +0700

    refactor: koneksi ke db tanpa pemberitahuan berhasil

--- config.php
+++ config.php
@@ -3,7 +3,5 @@ $conn = mysqli_connect("localhost", "root", "", "db_siswa");
 
 if (!$conn) {
     die("Gagal terhubung ke database: " . mysqli_connect_error());
-} else {
-    echo "Berhasil";
 }
 ?>
