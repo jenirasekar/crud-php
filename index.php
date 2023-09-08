af2d37aad448c8b7383d3d7f6f48d18895f75a6e
Jenira Sekar <jenirasekar@gmail.com>
2023-09-08 08:16:45 +0700

    refactor: import bootstrap via cdn

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data siswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -15,9 +18,9 @@
     </header>
     <nav><a href="v_form.php"><button>Tambah baru</button></a></nav>
     <br>
-    <table border="1" cellpadding="9">
+    <table class="table table-striped">
         <thead>
-            <tr>
+            <tr class="table table-success">
                 <th>NIS</th>
                 <th>Nama</th>
                 <th>Alamat</th>
